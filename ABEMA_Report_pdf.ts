fa07360e90c26361e3d1708bad41f674695a2cf6
kushi19944 <57084461+kushi19944@users.noreply.github.com>
2020-04-13 16:54:11 +0900

    Add files via upload

---
+++ ABEMA_Report_pdf.ts
@@ -0,0 +1,65 @@
+import RPA from 'ts-rpa';
+
+async function Start() {
+  await RPA.Google.authorize({
+    //accessToken: process.env.GOOGLE_ACCESS_TOKEN,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    tokenType: 'Bearer',
+    expiryDate: parseInt(process.env.GOOGLE_EXPIRY_DATE, 10),
+  });
+  // 実行前にダウンロードフォルダを全て削除する
+  await RPA.File.rimraf({ dirPath: `${process.env.WORKSPACE_DIR}` });
+  await RPA.Logger.info(RPA.File.outDir);
+  // 指定したスプシを pdf で保存
+  await GoogleLogIN();
+  // pdf をドライブにアップロードする
+  await DriveUPload();
+  await RPA.WebBrowser.quit();
+}
+
+Start();
+
+async function GoogleLogIN() {
+  // 指定したシートIDをpdfで保存
+  await RPA.WebBrowser.get(process.env.ABEMA_Report_pdf_URL);
+  await RPA.sleep(3000);
+  const IDinput = await RPA.WebBrowser.wait(
+    RPA.WebBrowser.Until.elementLocated({
+      css: '#identifierId',
+    }),
+    15000
+  );
+  await RPA.WebBrowser.sendKeys(IDinput, [`${process.env.ABEMA_ID}`]);
+  const NextButton1 = await RPA.WebBrowser.findElementById('identifierNext');
+  await NextButton1.click();
+  await RPA.sleep(3000);
+  const PWInput = await RPA.WebBrowser.wait(
+    RPA.WebBrowser.Until.elementLocated({
+      name: 'password',
+    }),
+    15000
+  );
+  await RPA.WebBrowser.sendKeys(PWInput, [`${process.env.ABEMA_PW}`]);
+  const NextButton2 = await RPA.WebBrowser.findElementById(`passwordNext`);
+  await NextButton2.click();
+  await RPA.sleep(10000);
+}
+
+async function DriveUPload() {
+  const FileNames = await RPA.File.list();
+  var today = new Date();
+  const ToDay =
+    today.getFullYear() + `-` + (today.getMonth() + 1) + `-` + today.getDate();
+  for (let i in FileNames) {
+    if (FileNames[i].includes('.pdf') == true) {
+      // pdfファイルの名前を日付に変更
+      await RPA.File.rename({ old: `${FileNames[i]}`, new: `${ToDay}.pdf` });
+      break;
+    }
+  }
+  RPA.Logger.info(`＊＊＊ pdfアップロード 開始＊＊＊`);
+  await RPA.Google.Drive.upload({
+    parents: [`${process.env.ABEMA_Report_pdf_DriveID}`],
+    filename: `${ToDay}.pdf`,
+  });
+}
